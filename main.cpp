f9ab0be4682b3e18af8641a96f01d28ff766f56b
Andrea <andreapappacoda02@gmail.com>
2020-05-27 14:28:27 +0200

    Set up glm w/CMake and pasted in some code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+// -I/usr/lib/llvm-10/include/c++/v1
 #include <vulkan/vulkan.hpp>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Paolo\n";
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << "paolo\n";
+	std::cout << extensionCount << " extensions supported\n";
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 }
